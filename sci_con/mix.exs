8aa66747c432a7dea8698e07dc244e0d283d7d2b
Matt Caldwell <StoatPower@users.noreply.github.com>
2026-03-22 19:31:31 -0400

    added initial mix.exs code for ex docs etc

--- sci_con/mix.exs
+++ sci_con/mix.exs
@@ -1,13 +1,23 @@
 defmodule SciCon.MixProject do
   use Mix.Project
 
+  @description "SciCon is a library of scientific constants."
+  @source_url "https://github.com/metacode-io/sci_con"
+  @version "0.1.0"
+
   def project do
     [
       app: :sci_con,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "SciCon",
+      description: @description,
+      source_url: @source_url,
+      docs: &docs/0
     ]
   end
 
@@ -24,6 +34,37 @@ defmodule SciCon.MixProject do
       {:req, "~> 0.5.16", only: [:dev, :test], runtime: false},
       {:nimble_parsec, "~> 1.4.2", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.40.1", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      language: "en",
+      formatters: ["html"],
+      main: "SciCon",
+      groups_for_modules: [
+        CODATA: [
+          SciCon.CODATA.Metadata,
+          SciCon.CODATA.Constants
+        ],
+        Codegen: [
+          CODATA: [
+            Scicon.Codegen.CODATA
+          ]
+        ]
+      ]
+    ]
+  end
+
+  defp package() do
+    [
+      maintainers: ["Metacode LLC"],
+      licenses: ["Apache-2.0"],
+      files:
+        ~w(CHANGELOG.md CODE_OF_CONDUCT.md CONTRIBUTING.md .formatter.exs lib dev LICENSE* mix.exs README.md),
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
